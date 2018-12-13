31ad0ed2ac127852f1877bb0882d2485acbc28e9
Nicholas Westerhausen <nwesterhausen@gmail.com>
2018-12-13 11:57:13 -0500

    Created an example config.js Ignore anyone's actual config.js

---
+++ sample-config.js
@@ -0,0 +1,8 @@
+// Set default
+let submitTopic = "mqtt-webform/submissions";
+let statusTopic = "mqtt-webform/status";
+let lastWillMessage = "offline";
+let birthMessage = "online";
+let websocketPort = 9001;
+let brokerHost = "localhost";
+let clientID = "mqtt-webform";
