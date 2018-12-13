52c850b7613bd4cfa8c22e78b596b06b293ac3ee
Nicholas Westerhausen <nicholas@westerhausen.me>
2018-12-13 07:41:18 -0500

    Created files.

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+* Provide a connection object
+* Show status of connection (it should try to connect when loaded)
+* Be able to set TOPIC
+* Be able to set CONNECTION VARS
+* Format form to JSON to publish
+* Publish JSON to topic, use QOS 1
+* ?? Verify that publish happened before clearing + success message
+*/
